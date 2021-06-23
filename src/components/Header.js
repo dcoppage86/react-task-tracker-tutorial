6c71ba0286137bcfd317725eac01b6528ca381c0
Dakota <dakotakcoppage@me.com>
2021-06-22 20:47:34 -0600

    started build with initial components and removed initial react info

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import Button from './Button'
+
+const Header = ({title}) => {
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Hello'/>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+export default Header
