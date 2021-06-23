c4e6e780a246934339ee5ef5fded2683ece1bd8a
Dakota <dakotakcoppage@me.com>
2021-06-23 09:27:24 -0600

    added functionality to toggle task form

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,25 @@
 import PropTypes from 'prop-types'
 
-const Button = ({ color, text }) => {
-    const onClick = (e) => {
-        console.log('click')
-    }
-    return <button onClick={onClick} style={{backgroundColor: color }} className='btn'>{text}</button>
+const Button = ({ color, text, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className='btn'
+    >
+      {text}
+    </button>
+  )
 }
 
 Button.defaultProps = {
-    color: 'steelblue'
+  color: 'steelblue',
 }
 
-Button.protoType = {
-    text: PropTypes.string,
-    color: PropTypes.string,
-    onClick: PropTypes.func,
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
 }
+
 export default Button
